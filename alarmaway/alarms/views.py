fd78ee3b354ed9df6f4848d5de3adf6a6e3c0957
Joe Roszkowski <Joe@JoeRoszkowski.com>
2013-01-31 17:15:58 -0500

    Updated logging: alarms.views

--- alarmaway/alarms/views.py
+++ alarmaway/alarms/views.py
@@ -1,5 +1,4 @@
 from __future__ import absolute_import, division, print_function
-import logging
 from flask import (Blueprint, flash, g, redirect, render_template,
     request, url_for
 )
@@ -12,6 +11,7 @@ from .forms import AddUserAlarmForm
 from .models import Alarm
 
 mod = Blueprint('alarms', __name__, url_prefix='/alarms')
+import logging.getLogger
 logger = logging.getLogger('alarmaway')
 
 @mod.route('/add', methods=['GET', 'POST'])
@@ -45,10 +45,10 @@ def add():
         except:
             #TODO Add correct exception handling
             flash("Could not add alarm, please try again.", 'error')
-            return redirect(url_for('users.home'))
-        logger.debug('New alarm created, %s' % alarm)
-        task_manager.processSetAlarm(alarm)
-        flash('Your alarm has been created and set', 'success')
+        else:
+            logger.info("New alarm created {}".format(alarm))
+            task_manager.processSetAlarm(alarm)
+            flash('Your alarm has been created and set', 'success')
         return redirect(url_for('users.home'))
     return render_template('alarms/add.html', form=form)
 
@@ -72,20 +72,14 @@ def remove(alarm_id):
 @mod.route('/update/<alarm_id>', methods=['GET', 'POST'])
 @login_required
 def update(alarm_id):
-    info_msg = ("""
-        VIEW NOT IMPLEMENTED :: alarms.update
-        alarm_id: %s, method type: %s"""
-        % (alarm_id, request.method)
-        )
-    logger.debug(info_msg)
-    return info_msg
+    logger.info("Update alarm {} view called by user {}".format(alarm_id, g.user.id))
+    return redirect(url_for('users.home'))
 
 @mod.route('/set/<alarm_id>')
 @login_required
 def set(alarm_id):
     """Basic view to set the current user's requested alarm."""
 
-    logger.debug('View set alarm %s' % alarm_id)
     alarm = Alarm.query.filter_by(id=alarm_id, owner=g.user).first_or_404()
     if alarm.active:
         flash("That alarm is already set.", 'error')
